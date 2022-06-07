aed060ecb990e97e79ddc36b8bdd8c6376b25284
Ezequiel <ezequieljsosa@gmail.com>
2022-06-06 22:12:29 -0300

    fix test

--- src/test/java/ar/edu/utn/link/correlativas/AlumnoTest.java
+++ src/test/java/ar/edu/utn/link/correlativas/AlumnoTest.java
@@ -2,6 +2,9 @@ package ar.edu.utn.link.correlativas;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import ar.edu.utn.link.correlativas.model.Alumno;
+import ar.edu.utn.link.correlativas.model.Curso;
+import ar.edu.utn.link.correlativas.model.Materia;
 import org.junit.jupiter.api.Test;
 
 public class AlumnoTest {
@@ -10,7 +13,7 @@ public class AlumnoTest {
 	public void testInscripcionOK() throws Exception {
 		// Pre
 		Alumno alumno = new Alumno("Ezequiel");
-		Curso curso = new Curso( new Materia("SO"),2022  );
+		Curso curso = new Curso( new Materia("SO",2022  ),2022);
 				
 		//operatoria
 		alumno.inscribir(curso);
